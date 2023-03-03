8f1dc431c3782fa7cae8dae79a16f6b07253411a
Max-Mackey <maxmackey@gmail.com>
2023-03-03 10:28:22 -0800

    Implemented div generation to create board grid which will be used to draw. Made a working reset button to clear the board of generated divs

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
-// captures the user input once submit is pressed and assings it to the variable "size"
-const button = document.querySelector('.submit');
-button.addEventListener('click', function() {
-    const theGridSize = document.querySelector('.gridSize');
-    const fieldValue = theGridSize.value;
-    //converts string to int
-    let size = parseInt(fieldValue);
-    //ensures that user chooses a number of 4 through 64
-    //reset size to zero if incorrect choice is made
-    if (size < 4) {
-        size = 0;
-        alert('Too low. Please choose 4 through 64 as a size.');
-    }
-    else if (size > 64) {
-        size = 0;
-        alert('Too high. Please choose 4 through 64 as a size. ')
-    }
-    else {
-        size = parseInt(fieldValue);
-    }
-});
+document.addEventListener("DOMContentLoaded", function() {
+    let size = 0;
+    // captures the user input once submit is pressed and assings it to the variable "size"
+    const button = document.querySelector('.submit');
+    button.addEventListener('click', function() {
+        const theGridSize = document.querySelector('.gridSize');
+        const fieldValue = theGridSize.value;
+        //converts string to int
+        const parsedValue = parseInt(fieldValue);
+        size = (parsedValue * parsedValue);
+        console.log(size);
+        //ensures that user chooses a number of 4 through 64 reset size to zero if incorrect choice is made
+        if (size < 16) {
+            size = 0;
+            alert('Too low. Please choose 4 through 64 as a size.');
+        }
+        else if (size > 4096) {
+            size = 0;
+            alert('Too high. Please choose 4 through 64 as a size. ')
+        }
+        const board = document.querySelector('.board');
+        board.style.setProperty('--cols', parsedValue);
+        board.style.setProperty('--rows', parsedValue);
+        console.log(parsedValue);
+    });
 
 
+//creates div boxes and appends them into the game board
+    const creation = document.querySelector('.submit');
+    creation.addEventListener('click', function() {
+        for (let i = 0; i < size; i++) {
+        const div = document.createElement('div');
+        div.style.color ='black';
+        div.style.padding = 0;
+        div.style.margin = 0;
+        div.style.backgroundColor = 'white';
+        div.textContent = "x";
+        document.querySelector('.board').appendChild(div)
+    }})
 
+    // Reset button. Wipes the board to it's original state
+    const eraseBoard = document.querySelector('.reset');
+    eraseBoard.addEventListener('click', function() {
+        const board = document.querySelector('.board');
+        board.innerHTML ='';
+    });
+})
